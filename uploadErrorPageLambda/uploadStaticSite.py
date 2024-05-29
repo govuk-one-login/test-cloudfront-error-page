bb2deed5fdea68ef28fa5846f1d5c9999790b55b
James Melville <james.melville@digital.cabinet-office.gov.uk>
2024-05-29 21:41:46 +0100

    fix decorators and move checks back to init

--- uploadErrorPageLambda/uploadStaticSite.py
+++ uploadErrorPageLambda/uploadStaticSite.py
@@ -2,61 +2,53 @@
 
 from __future__ import print_function
 from crhelper import CfnResource
+import os
 import logging
+import boto3
 
 logger = logging.getLogger(__name__)
 # Initialise the helper, all inputs are optional, this example shows the defaults
 helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120, ssl_verify=None)
 
-import os
-import boto3
-
 try:
-  s3=boto3.client('s3')
-  bucket = os.environ.get('BucketName')
-  with open ("static-error-pages/index.html") as f:
-    s3.upload_fileobj(f, bucket, "cloudfront")
+    s3 = boto3.client('s3')
+    logger.info("Created S3 client")
+
+    bucket = os.environ['BucketName']
+    logger.info("Got BucketName")
+
+    with open ("static-error-pages/index.html") as f:
+       pass
+    logger.info("Able to open static-error-pages/index.html")
+
 except Exception as e:
     helper.init_failure(e)
 
 @helper.create
-def create(event, context):
-    logger.info("Got Create")
-    # Optionally return an ID that will be used for the resource PhysicalResourceId, 
-    # if None is returned an ID will be generated. If a poll_create function is defined 
-    # return value is placed into the poll event as event['CrHelperData']['PhysicalResourceId']
-    #
-    # To add response data update the helper.Data dict
-    # If poll is enabled data is placed into poll event as event['CrHelperData']
-    helper.Data.update({"test": "testdata"})
-
-    # To return an error to cloudformation you raise an exception:
-    if not helper.Data.get("test"):
-        raise ValueError("this error will show in the cloudformation events log and console.")
-    
-    return "MyResourceId"
-
-
 @helper.update
-def update(event, context):
-    logger.info("Got Update")
-    # If the update resulted in a new resource being created, return an id for the new resource. 
-    # CloudFormation will send a delete event with the old id when stack update completes
+def updateBucket(event, context):
+  logger.info("Got Create or Update")
+  # Optionally return an ID that will be used for the resource PhysicalResourceId, 
+  # if None is returned an ID will be generated.
+  #
+  # To add response data update the helper.Data dict
+  # If poll is enabled data is placed into poll event as event['CrHelperData']
+
+  
+  with open ("static-error-pages/index.html") as f:
+
+    helper.Data.update(s3.upload_fileobj(f, bucket, "cloudfront"))
+
+    # # To return an error to cloudformation you raise an exception:
+    # if not helper.Data.get("test"):
+    #     raise ValueError("this error will show in the cloudformation events log and console.")
+
 
 
 @helper.delete
-def delete(event, context):
-    logger.info("Got Delete")
-    # Delete never returns anything. Should not fail if the underlying resources are already deleted.
-    # Desired state.
+def no_op(_, __):
+    pass
 
-
-@helper.poll_create
-def poll_create(event, context):
-    logger.info("Got create poll")
-    # Return a resource id or True to indicate that creation is complete. if True is returned an id 
-    # will be generated
-    return True
 
 
 def handler(event, context):
